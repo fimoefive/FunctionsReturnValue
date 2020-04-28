5835370398abcd84690c5ec844180c2adec2ea9d
martin sisk <martin.sisk@pop.belmont.edu>
2020-04-28 11:59:08 -0500

    first initial

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+
+// exercise 3
+/*  Write a function that takes a string of a dog breed as an argument (like 'border collie')
+
+Have the function return "my favorite dog breed is" 
+plus the passed in string. If no argument is passed to the function, return "I like cats".
+
+Execute the function in a way that captures the returned value in a variable.
+
+Console.log the string "When it comes to pets," plus the returned value of the function.*/
+
+function dogBreed() {
+    var favoriteDog =  'border collie';
+    var secondPet = 
+if ()
+if else null; 
+return "I like cats";
+
+console.log(`"When it comes to pets,"`);
